f76c4f6ad38acc0b834da4ec005b6cebae9b76c1
Andreas Luxenburger <luxenburger.andreas@gmail.com>
2015-01-15 19:35:56 +0100

    Second commit
    Now the source code even features an additional comment line!

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Now with even more improved comments!
 		System.out.println("Hello World!\n");
 	}
 
