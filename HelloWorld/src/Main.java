aa9c6d488c41e20d967a384ef27c99c50b9efdb6
Andreas Luxenburger <luxenburger.andreas@gmail.com>
2015-01-15 20:38:25 +0100

    Further chnages.

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// Now with even more improved comments!
+		// Now with even more improved comments!!
 		System.out.println("Hello World!\n");
 	}
 
